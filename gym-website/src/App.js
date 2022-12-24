990e942d32e9a58f3fafb9204bb82a6e2c572238
Edyane <edyanealves@gmail.com>
2022-12-24 17:34:08 -0300

    Construindo Hero Section

--- gym-website/src/App.js
+++ gym-website/src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Hero from "./components/UI/Hero"
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   )
 }
