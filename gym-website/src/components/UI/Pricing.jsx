c1003cd9e90ef2ee7abf03862aaa8571714c8051
Edyane <edyanealves@gmail.com>
2022-12-30 18:49:47 -0300

    Finalizando Pricing Section

--- gym-website/src/components/UI/Pricing.jsx
+++ gym-website/src/components/UI/Pricing.jsx
@@ -14,7 +14,7 @@ const Pricing = () => {
         <div className="pricing__wrapper">
             <div className="pricing__item">
                 <div className="pricing__card-top">
-                    <h2 className="section__title">Regular Members</h2>
+                    <h2 className="section__title">Regular Member</h2>
                     <h2 className="pricing section__title">$50 <span>/month</span></h2>
                 </div>
 
@@ -26,8 +26,47 @@ const Pricing = () => {
                         <li><span><i className="ri-checkbox-blank-circle-fill"></i></span> Standard options</li>
                         <li><span><i className="ri-checkbox-blank-circle-fill"></i></span> 5 classes per week</li>
                     </ul>
+                    <button className="register__btn">Join Now</button>
                 </div>
             </div>
+
+            <div className="pricing__item pricing__item-02">
+                <div className="pricing__card-top">
+                    <h2 className="section__title">Premium Member</h2>
+                    <h2 className="pricing section__title">$70 <span>/month</span></h2>
+                </div>
+
+                <div className="services">
+                    <ul>
+                        <li><span><i className="ri-checkbox-blank-circle-fill"></i></span> Unlimited access to the gym</li>
+                        <li><span><i className="ri-checkbox-blank-circle-fill"></i></span> Customer support</li>
+                        <li><span><i className="ri-checkbox-blank-circle-fill"></i></span> Personal trainer</li>
+                        <li><span><i className="ri-checkbox-blank-circle-fill"></i></span> Standard options</li>
+                        <li><span><i className="ri-checkbox-blank-circle-fill"></i></span> 5 classes per week</li>
+                    </ul>
+                    <button className="register__btn">Join Now</button>
+                </div>
+            </div>
+
+            <div className="pricing__item">
+                <div className="pricing__card-top">
+                    <h2 className="section__title">Standard Member</h2>
+                    <h2 className="pricing section__title">$100 <span>/month</span></h2>
+                </div>
+
+                <div className="services">
+                    <ul>
+                        <li><span><i className="ri-checkbox-blank-circle-fill"></i></span> Unlimited access to the gym</li>
+                        <li><span><i className="ri-checkbox-blank-circle-fill"></i></span> Customer support</li>
+                        <li><span><i className="ri-checkbox-blank-circle-fill"></i></span> Personal trainer</li>
+                        <li><span><i className="ri-checkbox-blank-circle-fill"></i></span> Standard options</li>
+                        <li><span><i className="ri-checkbox-blank-circle-fill"></i></span> 5 classes per week</li>
+                    </ul>
+                    <button className="register__btn">Join Now</button>
+                </div>
+            </div>
+
+            
         </div>
         </div>
     </section>
