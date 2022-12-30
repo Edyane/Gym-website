9f206a84039607f8542dcc1d053d67ece28476aa
Edyane <edyanealves@gmail.com>
2022-12-30 19:37:27 -0300

    Adicionando Testimonials

--- gym-website/src/App.js
+++ gym-website/src/App.js
@@ -5,6 +5,7 @@ import Hero from "./components/UI/Hero";
 import Exercises from "./components/UI/Exercises";
 import Start from "./components/UI/Start";
 import Pricing from "./components/UI/Pricing";
+import Testimonials from "./components/UI/Testimonials";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Exercises />
       <Start />
       <Pricing />
+      <Testimonials />
     </>
   )
 }
