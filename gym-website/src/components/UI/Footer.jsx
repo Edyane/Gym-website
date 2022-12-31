be4ba210cdec3fefaadf711bbd628a5dfe4b99a8
Edyane <edyanealves@gmail.com>
2022-12-30 22:22:50 -0300

    Realizando Ajustes nos Componentes

--- gym-website/src/components/UI/Footer.jsx
+++ gym-website/src/components/UI/Footer.jsx
@@ -3,51 +3,49 @@ import "../../styles/Footer.css"
 import logo from '../../assets/img/dumble.png';
 
 const Footer = () => {
-
-    const year = new Date().getFullYear
     return <footer className="footer">
         <div className="container">
             <div className="footer__wrapper">
                 <div className="footer__box">
                     <div className="logo">
                         <div className="logo__img">
-                            <img src={ logo } alt="" />
+                            <img src={logo} alt="" />
                         </div>
                         <h2>FitBody</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, porro alias.</p>
                     </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, dicta.</p>
+                </div>
 
-                    <div className="footer__box">
-                        <h4 className="footer__title">Company</h4>
-                        <ul className="footer__links">
-                            <li><a href="#">Our Program</a></li>
-                            <li><a href="#">Our Plan</a></li>
-                            <li><a href="#">Become a Member</a></li>
-                        </ul>
-                    </div>
+                <div className="footer__box">
+                    <h4 className="footer__title">Company</h4>
 
-                    <div className="footer__box">
-                        <h4 className="footer__title">Quick Links</h4>
-                        <ul className="footer__links">
-                            <li>
-                                <a href="#">About us</a></li>
-                            <li><a href="#">Contact us</a></li>
-                            <li><a href="#">Support</a></li>
-                        </ul>
-                    </div>
+                    <ul className="footer__links">
+                        <li><a href="#">Our program</a></li>
+                        <li><a href="#">Our plan</a></li>
+                        <li><a href="#">Become a member</a></li>
+                    </ul>
+                </div>
 
-                    <div className="footer__box">
-                        <h4 className="footer__title">Quick Links</h4>
-                        <ul className="footer__links">
-                            <li>
-                                <a href="#">About us</a></li>
-                            <li><a href="#">Contact us</a></li>
-                            <li><a href="#">Support</a></li>
-                        </ul>
-                    </div>
+                <div className="footer__box">
+                    <h4 className="footer__title">Quick Links</h4>
+
+                    <ul className="footer__links">
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Contact us</a></li>
+                        <li><a href="#">Support</a></li>
+                    </ul>
+                </div>
+
+                <div className="footer__box">
+                    <h4 className="footer__title">Plan</h4>
+
+                    <ul className="footer__links">
+                        <li><a href="#">Regular</a></li>
+                        <li><a href="#">Premium</a></li>
+                        <li><a href="#">Standard</a></li>
+                    </ul>
                 </div>
             </div>
-            <p className="copyright">Copyright</p>
         </div>
     </footer>
 };
